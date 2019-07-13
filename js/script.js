d4904246c2ab77b463024c99347423335d28c090
WebFlat <y.vacheslav@gmail.com>
2019-07-13 14:23:15 +0300

    final

--- js/script.js
+++ js/script.js
@@ -9,6 +9,12 @@ $(document).ready(function() {
         $('.tab').removeClass('active');
         $($(this).attr('href')).fadeIn();
         $(this).addClass('active');
-    })
+    });
     
+
+    var shape = document.getElementById('progress');
+    var shapeLength = shape.getTotalLength().toFixed(0) - 117;
+    var progressShow = shape.setAttribute('stroke-dashoffset', shapeLength);
+    	document.getElementById('percent').innerText = (shapeLength/10);
+
 });
